2c603c6b3f0eddf78e250f896f928e9117c27541
Ruben Juarez <juarez.ruben2606@gmail.com>
2024-09-22 10:00:18 -0500

    add new files and path and connecting with backend in index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,37 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const port = 3000;
-const app = express();
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
+app.get("/header", (req, res) => {
+  res.render("header");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/profile", (req, res) => {
+  res.render("profile");
+});
+
+app.get("/create", (req, res) => {
+  res.render("create");
+});
+
+app.get("/footer", (req, res) => {
+  res.render("footer");
+});
+// Start the server
 app.listen(port, () => {
   console.log(`Server started on port ${port}.`);
 });
